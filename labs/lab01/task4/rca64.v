9bfeef07b7231379eab147f35ee1ae344149141c
Ritvik Roy <f20240880@pilani.bits-pilani.ac.in>
2026-08-29 17:43:16 +0000

    lab01 task4

--- labs/lab01/task4/rca64.v
+++ labs/lab01/task4/rca64.v
@@ -27,22 +27,13 @@ module rca64(
 
   // TODO: your 64-bit ripple-carry structure goes here.
   wire [64:0] c;
-
-assign #(2) c[0] = cin;
-
-genvar i;
-generate
-  for (i = 0; i < 64; i = i + 1) begin : gen_fa
-    FA_Gate FA (
-      .a(a[i]),
-      .b(b[i]),
-      .cin(c[i]),
-      .sum(sum[i]),
-      .cout(c[i+1])
-    );
-  end
-endgenerate
-
-assign #(2) cout = c[64];
+  assign c[0] = cin;
+  genvar i;
+  generate
+    for (i = 0; i < 64; i = i + 1) begin : gen_fa
+      FA_Gate FA (.a(a[i]), .b(b[i]), .cin(c[i]), .sum(sum[i]), .cout(c[i+1]));
+    end
+  endgenerate
+  assign cout = c[64];
 
 endmodule
