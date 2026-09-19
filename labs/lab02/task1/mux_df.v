75726d1eaa97a2cd62be27100f04fcf4d2b3b9a5
Ritvik Roy <f20240880@pilani.bits-pilani.ac.in>
2026-09-19 15:51:02 +0000

    lab02 task1

---
+++ labs/lab02/task1/mux_df.v
@@ -0,0 +1,19 @@
+// mux_df.v
+// 2-to-1 multiplexer, DATAFLOW style.
+//
+// This file does not compile as-is. Find the bug and fix it before moving on.
+// Hint: think carefully about which port should be a net and which should be
+// a variable in dataflow modeling.
+
+module mux_df (
+  input      I0,
+  input      I1,
+  input      S,
+  // A continuous `assign` drives a net.  Declaring Y as reg here would make
+  // the simulator see a procedural variable being driven by a net driver.
+  output wire Y
+);
+
+  assign Y = S ? I1 : I0;
+
+endmodule
